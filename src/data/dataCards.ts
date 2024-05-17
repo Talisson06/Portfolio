d15e6cc35991ecc2acba04082a13e9b65527f722
Talisson Dias <126925961+Talisson06@users.noreply.github.com>
2024-05-17 13:56:39 -0300

    update

--- src/data/dataCards.ts
+++ src/data/dataCards.ts
@@ -1,10 +1,28 @@
 import { Cards } from "../types/Cards";
 
 export const dataCards: Cards[] = [
-    { id: 1, 
+    { id: 0, 
       url: '../src/assets/Praia.jpg', 
       title: 'Ponto Turistico Salvador', 
       tecnologys:[' JS ' , ' HTML ', ' CSS '],
       
+    },
+    { id: 1 , 
+      url:'../src/assets/instagram.png' , 
+      title:' StormSilver ' , 
+      tecnologys: [' HTML5 ', 'CSS3', ' JS '],
+
+    },
+    { id: 2 , 
+      url:'../src/assets/github.png' , 
+      title:' Equação do Segundo Grau ' , 
+      tecnologys: [' HTML5 ', 'CSS3', ' JS '],
+
+    },
+    { id: 3 , 
+      url:'../src/assets/linkedin.png' , 
+      title:' AcademiaZ ' , 
+      tecnologys: [' Nextjs ', 'SCSS', ' JS ', 'Typescript'],
+
     }
 ] 
